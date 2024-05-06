4dd164371478bbb5da1a917a1ebaf068f1e920fc
Sudhanshu2328 <sudhanshu6389@gmail.com>
2024-05-06 10:40:34 +0530

    .....

--- Backend/Models/Usermodel.js
+++
@@ -1,7 +0,0 @@
-const {Schema, model} = require('../Connection')
-const userSchema = new Schema({
-    name: String,
-    email: String,
-    password: String
-});
-module.exports = model('user', userSchema)
