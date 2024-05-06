2f90063394c89183bd30234d271bcfee462f743d
Hafsah1999 <hafsahnaseer233@gmail>
2024-05-06 14:45:43 +0530

    ...

--- Backend/Models/Usermodel.js
+++ Backend/Models/Usermodel.js
@@ -1,12 +1,12 @@
-const {Schema, model} = require('../Connection')
+const { Schema, model } = require('../Connection')
 const userSchema = new Schema({
-    name: String,
-    email: String,
-    password: String,
-    role: {
-        type: String,
-        default: "user",
-      },
-      avatar:{type:String, default:"Avatar.png"}
+  name: String,
+  email: String,
+  password: String,
+  role: {
+    type: String,
+    default: "user",
+  },
+  avatar: { type: String, default: "Avatar.png" }
 });
 module.exports = model('user', userSchema)
